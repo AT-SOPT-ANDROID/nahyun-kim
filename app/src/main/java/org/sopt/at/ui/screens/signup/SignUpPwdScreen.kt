f556540d8d4b4f0f4ca0dc7519f6f9d1ec4dbc63
Nahyun Kim <hedghog1004@naver.com>
2025-04-11 19:14:15 +0900

    [feat/#1] 회원가입 IME Padding 추가

--- app/src/main/java/org/sopt/at/ui/screens/signup/SignUpPwdScreen.kt
+++ app/src/main/java/org/sopt/at/ui/screens/signup/SignUpPwdScreen.kt
@@ -7,6 +7,7 @@ import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.imePadding
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
 import androidx.compose.material3.Icon
@@ -54,7 +55,8 @@ fun SignUpPwdScreen(
                 vertical = dimensionResource(R.dimen.screen_padding_vertical),
                 horizontal = dimensionResource(R.dimen.screen_padding_horizontal)
             )
-            .fillMaxSize(),
+            .fillMaxSize()
+            .imePadding(),
         horizontalAlignment = Alignment.CenterHorizontally,
         verticalArrangement = Arrangement.SpaceBetween
     ) {
