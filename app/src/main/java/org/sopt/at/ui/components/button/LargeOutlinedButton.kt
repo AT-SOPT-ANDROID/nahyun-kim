99479f5e3245e042f8ca31ac678b29732719d0fb
Nahyun Kim <hedghog1004@naver.com>
2025-04-11 18:31:13 +0900

    [feat/#1] OutlinedButton 리플 효과 제거

--- app/src/main/java/org/sopt/at/ui/components/button/LargeOutlinedButton.kt
+++ app/src/main/java/org/sopt/at/ui/components/button/LargeOutlinedButton.kt
@@ -2,12 +2,16 @@ package org.sopt.at.ui.components.button
 
 import androidx.annotation.StringRes
 import androidx.compose.foundation.BorderStroke
+import androidx.compose.foundation.indication
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.dimensionResource
 import androidx.compose.ui.res.stringResource
@@ -19,6 +23,7 @@ import org.sopt.at.R
 import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
 import org.sopt.at.ui.theme.ButtonDisableText
 import org.sopt.at.ui.theme.LabelButtonText
+import org.sopt.at.ui.util.DisableRippleEffect
 
 @Composable
 fun LargeOutlinedButton(
@@ -26,20 +31,29 @@ fun LargeOutlinedButton(
     @StringRes buttonTextRes: Int = R.string.next,
     onClick: () -> Unit = {}
 ) {
-    OutlinedButton(
-        modifier = modifier
-            .fillMaxWidth(),
-        onClick = onClick,
-        border = BorderStroke(dimensionResource(R.dimen.outline_button_stroke_width), ButtonDisableText),
-        shape = RoundedCornerShape(dimensionResource(R.dimen.button_radius)),
-        contentPadding = PaddingValues(vertical = 14.dp)
-    ) {
-        Text(
-            text = stringResource(buttonTextRes),
-            fontSize = 14.sp,
-            fontWeight = FontWeight.ExtraBold,
-            color = LabelButtonText
-        )
+    DisableRippleEffect {
+        OutlinedButton(
+            modifier = modifier
+                .fillMaxWidth()
+                .indication(
+                    interactionSource = remember { MutableInteractionSource() },
+                    indication = null
+                ),
+            onClick = onClick,
+            border = BorderStroke(
+                dimensionResource(R.dimen.outline_button_stroke_width),
+                ButtonDisableText
+            ),
+            shape = RoundedCornerShape(dimensionResource(R.dimen.button_radius)),
+            contentPadding = PaddingValues(vertical = 14.dp)
+        ) {
+            Text(
+                text = stringResource(buttonTextRes),
+                fontSize = 14.sp,
+                fontWeight = FontWeight.ExtraBold,
+                color = LabelButtonText
+            )
+        }
     }
 }
 
