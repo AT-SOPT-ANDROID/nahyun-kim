c50c367d069163b6217d28aa738a239db7facbd9
Nahyun Kim <hedghog1004@naver.com>
2025-04-27 14:33:24 +0900

    [refactor/#3] TabLayout 구현 방식 변경
    - ScrollableTabRow 사용 대신 직접 구현

--- app/src/main/java/org/sopt/at/presentation/home/HomeScreen.kt
+++ app/src/main/java/org/sopt/at/presentation/home/HomeScreen.kt
@@ -5,14 +5,8 @@ import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.material3.LocalContentColor
-import androidx.compose.material3.MaterialTheme
-import androidx.compose.material3.ScrollableTabRow
-import androidx.compose.material3.Tab
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.ui.Alignment
@@ -25,13 +19,11 @@ import androidx.lifecycle.viewmodel.compose.viewModel
 import kotlinx.serialization.Serializable
 import org.sopt.at.R
 import org.sopt.at.presentation.home.component.BannerCarousel
+import org.sopt.at.presentation.home.component.HomeTabLayout
 import org.sopt.at.presentation.home.component.HomeTopBar
 import org.sopt.at.presentation.home.component.RecommendContent
 import org.sopt.at.presentation.home.model.TabGenreResource
 import org.sopt.at.ui.theme.ATSOPTANDROIDTheme
-import org.sopt.at.ui.theme.Background
-import org.sopt.at.ui.theme.ButtonDisableText
-import org.sopt.at.ui.theme.White
 
 @Serializable
 data object Home
@@ -45,7 +37,7 @@ fun HomeScreen(
     navigationToMy: (id: String) -> Unit
 ) {
     val tabTitles = stringArrayResource(R.array.home_tab_array)
-    var selectedTabIndex = viewModel.selectedTabIndex.collectAsState()
+    var selectedTabIndex = viewModel.selectedTabIndex.collectAsState().value
     Column(
         modifier = Modifier
             .padding(paddingValues)
@@ -58,30 +50,30 @@ fun HomeScreen(
             onProfileClick = { navigationToMy(userId) }
         )
         LazyColumn(
-            verticalArrangement = Arrangement.spacedBy(20.dp),
+            verticalArrangement = Arrangement.spacedBy(16.dp),
             contentPadding = PaddingValues(vertical = 8.dp),
             modifier = Modifier
                 .fillMaxSize()
         ) {
             stickyHeader {
-                HomeScrollableTabRow(
-                    tabTitles = tabTitles.toList(),
-                    selectedTabIndex = selectedTabIndex.value,
-                ) { tabIndex ->
-                    viewModel.selectTab(tabIndex)
-                }
+                HomeTabLayout(
+                    selectedTabIndex = selectedTabIndex,
+                    onTabClick = { tabIndex ->
+                        viewModel.selectTab(tabIndex)
+                    }
+                )
             }
             item {
                 BannerCarousel(
-                    bannerImageUrls = TabGenreResource.getDataById(selectedTabIndex.value)!!.bannerImages,
+                    bannerImageUrls = TabGenreResource.getDataById(selectedTabIndex)!!.bannerImages,
                 )
             }
             item {
                 RecommendContent(
                     modifier = Modifier,
-                    title = stringResource(R.string.home_recommend_live_popular, tabTitles[selectedTabIndex.value]),
+                    title = stringResource(R.string.home_recommend_live_popular, tabTitles[selectedTabIndex]),
                     isSupportRanking = true,
-                    imageUrls = TabGenreResource.getDataById(selectedTabIndex.value)!!.posterImages,
+                    imageUrls = TabGenreResource.getDataById(selectedTabIndex)!!.posterImages,
                     isShowMoreButton = false
                 )
             }
@@ -91,42 +83,9 @@ fun HomeScreen(
                     title = stringResource(R.string.home_recommend_current_broadcast),
                     isSupportRanking = false,
                     isShowMoreButton = true,
-                    imageUrls = TabGenreResource.getDataById(selectedTabIndex.value)!!.posterImages.reversed()
+                    imageUrls = TabGenreResource.getDataById(selectedTabIndex)!!.posterImages.reversed()
                 )
             }
-        }
-    }
-}
-
-@Composable
-fun HomeScrollableTabRow(
-    modifier: Modifier = Modifier,
-    tabTitles: List<String>,
-    selectedTabIndex: Int,
-    onTabClick: (Int) -> Unit
-) {
-    ScrollableTabRow(
-        modifier = modifier.fillMaxWidth(),
-        selectedTabIndex = selectedTabIndex,
-        containerColor = Background,
-        edgePadding = 0.dp,
-        indicator = {}, // 인디케이터 제거
-        divider = {}, // 경계선 제거
-    ) {
-        tabTitles.forEachIndexed { tabIndex, tab ->
-            Tab(
-                selected = selectedTabIndex == tabIndex,
-                onClick = { onTabClick(tabIndex) },
-                selectedContentColor = White,
-                unselectedContentColor = ButtonDisableText,
-                text = {
-                    Text(
-                        text = tab,
-                        style = MaterialTheme.typography.bodyMedium,
-                        color = LocalContentColor.current,
-                    )
-                },
-            )
         }
     }
 }
